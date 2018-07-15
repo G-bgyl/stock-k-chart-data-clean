8b3efb07e4932024e32fcf71e1e7014acb7dca03
G-bgyl <linyange@umich.edu>
2018-07-15 16:20:55 +0800

    [update] distinguish different type of stock file

--- clean_kchart.py
+++ clean_kchart.py
@@ -42,31 +42,47 @@ class Clean_stock_data():
         # get a list of names
         self.stock_names=self.df['StockID'].unique().tolist()
 
-    def remove_useless_row(self, sr = False):
+    def remove_useless_row(self, type = False):
 
-
-        # delete useless row that exceed the bound of time
         self.joe = self.joe.set_index('date')
         self.joe.index = pd.to_datetime(self.joe.index)
-        if sr:
-            joe1 = self.joe.between_time('08:59:00', '15:00:59')
-            joe2 = self.joe.between_time('20:59:00', '2:30:59')
-            frames = [joe1, joe2]
-        else:
-            joe1 = self.joe.between_time('08:59:00', '15:00:59')
-            joe2 = self.joe.between_time('20:59:00', '2:30:59')
-            frames = [joe1, joe2]
+        # delete useless row that exceed the bound of time
 
-        self.joe = pd.concat(frames)
+        if type == 'SR':
+            joe1 = self.joe.between_time('08:59:00', '15:00:59')
+            joe2 = self.joe.between_time('20:59:00', '2:30:59')
+            frames = [joe1, joe2]
+            self.joe = pd.concat(frames)
+            merge_begin0 = ' 08:59'
+            merge_begin1 = ' 09:00'
+            merge_late0 = ' 20:59'
+            merge_late1 = ' 21:00'
+        elif type == 'OP':
+            self.joe = self.joe.between_time('09:29:00', '15:00:59')
+            merge_begin0 = ' 09:29'
+            merge_begin1 = ' 09:30'
+            merge_late0 = None
+            merge_late1 = None
+        else:
+            joe1 = self.joe.between_time('09:29:00', '15:00:59')
+            joe2 = self.joe.between_time('20:59:00', '2:30:59')
+            frames = [joe1, joe2]
+            self.joe = pd.concat(frames)
+            merge_begin0 = ' 09:29'
+            merge_begin1 = ' 09:30'
+            merge_late0 = ' 20:59'
+            merge_late1 = ' 21:00'
+
         self.joe = self.joe[self.joe.buy1 != 0]
         self.joe = self.joe[self.joe.sale1 != 0]
         # self.joe = self.joe[self.joe.price != 0]
 
-
+        return merge_begin0, merge_begin1, merge_late0, merge_late1
 
     def get_tstamp(self):
         try:
-            # create minure column
+
+            # create minute column
             self.joe['minute'] = self.joe.index
             self.joe['minute'] = self.joe.minute.apply(str)
             self.joe['minute'] = self.joe['minute'].str.slice(stop=-3)
@@ -83,7 +99,7 @@ class Clean_stock_data():
         else:
             return result
 
-    def get_last(self,each):
+    def get_data(self,each):
         # cal
         open_p = each.iloc[0]['open']
 
@@ -104,6 +120,15 @@ class Clean_stock_data():
         askvol1 = last['sc1']
         return [open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1]
 
+    def together_begin(self,early, minute_):
+        open_, high_, low_, close_, volume_, opint_, bid1_, ask1_, bidvol1_, askvol1_ = early[1:]
+        open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1 = minute_[1:]
+        open_p = open_  # use early one
+        high = max(high, high_)
+        low = min(low, low_)
+        volume = sum([volume_, volume])
+        minute_[1:] = [open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1]
+
     def Cal_k(self, path):
 
         #  iterate through each stock
@@ -112,10 +137,12 @@ class Clean_stock_data():
             self.joe = self.df.loc[self.df['StockID'] == name]
 
             minutes = []
-
+            type = name[:2]
+            merge_begin0, merge_begin1, merge_late0, merge_late1 =self.remove_useless_row(type)
 
             # get all stock names
             grouped = self.get_tstamp()
+
             early = None
             late = None
             if grouped:
@@ -128,48 +155,42 @@ class Clean_stock_data():
                     # loop through each minute
                     for key,each in grouped :
                         self. joe = each
-                        self.remove_useless_row()
+
                         minute_ = []
-                        minute_.append(key)
-                        result = self.get_last(each)
+                        minute_.append(str(key))
+                        result = self.get_data(each)
                         minute_.extend(result)
-                        if ' 08:59' in minute_[0]:
+                        if merge_begin0 in minute_[0]:
                             early = minute_
-                        if ' 20:59' in minute_[0]:
+                        if merge_late0 and merge_late0 in minute_[0]:
                             late = minute_
                         minutes.append(minute_)
 
                     # write rows
                     for minute_ in minutes:
 
-                        if ' 09:00' in minute_[0]:
+                        if merge_begin1 in minute_[0]:
                             # turn 8:59 and 9:00 into one
                             if early is not None:
-                                open_, high_, low_, close_, volume_, opint_, bid1_, ask1_, bidvol1_, askvol1_ = early[1:]
-                                open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1 = minute_[1:]
-                                open_p = open_ #  use early one
-                                high = max(high,high_)
-                                low = min(low,low_)
-                                volume = sum([volume_,volume])
-                                minute_[1:] = [open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1]
-                        if ' 21:00' in minute_[0]:
+                                self.together_begin(early,minute_)
+                        if merge_late1 and merge_late1 in minute_[0]:
                             # turn 20:59 and 21:00 into one
                             if late is not None:
-                                open_, high_, low_, close_, volume_, opint_, bid1_, ask1_, bidvol1_, askvol1_ = late[1:]
-                                open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1 = minute_[1:]
-                                open_p = open_  # use early one
-                                high = max(high, high_)
-                                low = min(low, low_)
-                                volume = sum([volume_, volume])
-                                minute_[1:] = [open_p, high, low, close, volume, opint, bid1, ask1, bidvol1, askvol1]
+                                self.together_begin(late, minute_)
                         # ignore ' 08:59' and ' 20:59'
-                        if ' 08:59' not in minute_[0] and ' 20:59' not in minute_[0]:
-                            # minute_[0]+=':00'
-                            minute_[1:-2] = [round(num,4) for num in minute_[1:-2]]
-                            writer.writerow(minute_)
+                        if merge_late0:
+                            if merge_begin0 not in minute_[0] and merge_late0 not in minute_[0]:
+                                # minute_[0]+=':00'
+                                minute_[1:-2] = [round(num,4) for num in minute_[1:-2]]
+                                writer.writerow(minute_)
+                        else:
+                            if merge_begin0 not in minute_[0]:
+                                # minute_[0]+=':00'
+                                minute_[1:-2] = [round(num,4) for num in minute_[1:-2]]
+                                writer.writerow(minute_)
 
     def main(self):
-        result_p = os.getcwd()+'/kchart_output/1w/'
+        result_p = os.getcwd()+'/1w_output/'
         # concat all the file together
         fnames = self.get_fname()
 
@@ -177,7 +198,8 @@ class Clean_stock_data():
             dir_name = names[0][:-6]
             # p = os.getcwd()
             date_path = '%s%s/'%(result_p,dir_name)
-            os.mkdir(date_path, 0o777)
+            if not os.path.exists(date_path):
+                os.mkdir(date_path, 0o777)
             self.get_df(names)
             # get index of different stocks
             self.get_stocks()
@@ -185,6 +207,6 @@ class Clean_stock_data():
             self.Cal_k(date_path)
 
 if __name__ =='__main__':
-    path = os.getcwd()+'/kchart_data/clean_1w/'
+    path = os.getcwd()+'/1w_data/'
     c = Clean_stock_data(path)
     c.main()
